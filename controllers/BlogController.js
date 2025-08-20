90502f7a47286b9523ef49ee2884488e3a4aba8f
Ian Nakamura <16380276688@estudante.ufjf.br>
2025-08-20 19:45:50 -0300

    Criação de formulario e formularioPOST

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -1,4 +1,4 @@
-const Blog = require('../models/Blog') // importacao do model Blog
+const Postagem = require('../models/Blog') // importacao do model Blog
 
 module.exports = class BlogController{ //exportacao da classe
     static async renderizaBlog(req,res){  //funcao para renderizar a página inicial | estatico nao precisa de um objeto da classe para ser chamada
@@ -6,5 +6,18 @@ module.exports = class BlogController{ //exportacao da classe
     }
     static async renderizaPostagens(req,res){
         res.render('blogs/postagens')
+    }
+    static async formulario(req,res){
+        res.render('blogs/formulario')
+    }
+    static async formularioPOST(req,res){
+        titulo = req.body.titulo
+        tema = req.body.tema
+        autor = req.body.autor
+        descricao = req.body.descricao
+
+        const postagem = new Postagem(titulo, tema, autor, descricao)
+        postagem.save()
+        res.redirect('/blog/postagens')
     }
 }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -6,5 +6,6 @@ const BlogController = require('../controllers/BlogController')
 
 router.get('/', BlogController.renderizaBlog)
 router.get('/postagens', BlogController.renderizaPostagens)
-
+router.get('/criarconteudo', BlogController.formulario)
+router.get('/criarconteudo', BlogController.formularioPOST)
 module.exports = router
