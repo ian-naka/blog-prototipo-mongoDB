d3604f8d966ccc8a4cc2f9461d39d2d4f53fce65
Ian Nakamura <16380276688@estudante.ufjf.br>
2025-08-20 09:59:24 -0300

    Criação da Rota /postagens

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -4,4 +4,7 @@ module.exports = class BlogController{ //exportacao da classe
     static async renderizaBlog(req,res){  //funcao para renderizar a página inicial | estatico nao precisa de um objeto da classe para ser chamada
         res.render('blogs/home')
     }
+    static async renderizaPostagens(req,res){
+        res.render('blogs/postagens')
+    }
 }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 const BlogController = require('../controllers/BlogController')
 
 router.get('/', BlogController.renderizaBlog)
+router.get('/postagens', BlogController.renderizaPostagens)
 
 module.exports = router
