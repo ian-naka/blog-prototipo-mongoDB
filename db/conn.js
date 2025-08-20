64c2d897fb60f57cd7ac4cff60b69ffcec9969ff
Ian Nakamura <16380276688@estudante.ufjf.br>
2025-08-20 09:53:04 -0300

    Configuração com o MongoDB

---
+++ db/conn.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require ('mongodb')
+const uri = 'mongodb://localhost:27017/blog'
+const client = new MongoClient(uri)
+
+async function run(){
+    try {
+        await client.connect()
+        console.log('Conectamos ao MongoDB')
+    } catch (error) {
+        console.log(`Deu erro na conexão ao banco, no arquivo conn.js: ${err}`)
+    }
+}
+run()
+
+module.exports = client
