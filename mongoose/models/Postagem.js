5e75d8b138a7a2db08161e67b3891561c9ed9e9f
Ian Nakamura <16380276688@estudante.ufjf.br>
2025-08-23 11:37:24 -0300

    Criação do Model

--- mongoose/models/Postagem.js
+++ mongoose/models/Postagem.js
@@ -1,42 +1,15 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
-const { ObjectId } = require('mongodb')
-const conn = require('../db/conn')
-
-class Postagem{
-
-    constructor(titulo, tema, autor, descricao){
-        this.titulo = titulo
-        this.tema = tema
-        this.autor = autor 
-        this.descricao = descricao
-    }
-    save(){
-        const postagem = conn.db().collection('Postagem').insertOne({
-            titulo: this.titulo,
-            tema: this.tema,
-            autor: this.autor,
-            descricao: this.descricao
-        })
-        return postagem
-    } //metodo para salvar dados
-
-    static getPostagens(){
-        const postagens = conn.db().collection('Postagem').find().toArray()
-        return postagens
-    } // cria o metodo static para resgatar as postagens
-
-    static async getPostagensId(id){
-        const postagem = await conn.db().collection('Postagem').findOne({ _id: new ObjectId(id)})
-        return postagem
-    }
-    static async removePostagemId(id){
-        const postagem = await conn.db().collection('Postagem').deleteOne({ _id: new ObjectId(id)})
-        return postagem
-    }
-    async atualizarPostagem(id){
-        await conn.db().collection('Postagem').updateOne({ _id: new ObjectId(id)}, {$set: this})
-        return 
-    }
-}
+const Postagem = mongoose.model(
+    'Postagem',
+    new Schema({
+        titulo: {type: String, required: true},
+        autor: {type: String, required: true},
+        descricao: {type: String, required: true},
+        tema: {type: String, required: true
+        }
+    }),
+)
 
 module.exports = Postagem
